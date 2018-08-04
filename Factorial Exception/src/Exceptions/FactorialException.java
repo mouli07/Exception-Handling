99c5b48e5dd44da1dd56a73d338cb9cd3a463922
unknown <moulimadas372@gmail.com>
2018-08-04 18:58:18 +0530

    added in new repo

---
+++ Factorial Exception/src/Exceptions/FactorialException.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+	public class FactorialException extends Exception
+	
+	{
+	private static final long serialVersionUID = 1L;
+	public FactorialException(String name) {
+	super(name);
+	}
+	
+
+}
